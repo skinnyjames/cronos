456c7e301e75bbd02c40a9bc0f16c12e2d8763aa
Sean Gregory <seangregory@protonmail.com>
2018-05-16 11:59:11 -0500

    standard

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const JAVASCRIPT_FLOOR = -100,000,000 days before january 1, 1970
+// const JAVASCRIPT_FLOOR = -100,000,000 days before january 1, 1970
 
 const SECONDS_PER_MINUTE = 60
 const SECONDS_PER_HOUR = 3600
@@ -7,7 +7,7 @@ const SECONDS_PER_WEEK = 604800
 const SECONDS_PER_MONTH = 2629746
 const SECONDS_PER_YEAR = 31536000
 
-const HUNDRED = 100 
+const HUNDRED = 100
 const THOUSAND = 1000
 const MILLION = 1000000
 
@@ -17,31 +17,26 @@ const SETTINGS = [
   { ratio: SECONDS_PER_WEEK, property: 'weeks' },
   { ratio: SECONDS_PER_DAY, property: 'days' },
   { ratio: SECONDS_PER_HOUR, property: 'hours' },
-  { ratio: SECONDS_PER_MINUTE, property: 'minutes' }, 
+  { ratio: SECONDS_PER_MINUTE, property: 'minutes' }
 ]
 
 module.exports = createHourGlass
 
-function HourGlass(time, reference) {
-
-  if (typeof time == 'object') {
-
+function HourGlass (time, reference) {
+  if (typeof time === 'object') {
     let seconds = time.seconds ? time.seconds : 0
 
-    for (let i=0;i<SETTINGS.length;i++) {
+    for (let i = 0; i < SETTINGS.length; i++) {
       if (time[SETTINGS[i].property]) {
         seconds += time[SETTINGS[i].property] * SETTINGS[i].ratio
-      } 
+      }
     }
 
     this.time = seconds
     this.reference = time.from
-
   } else {
-
     this.time = time
     this.reference = reference
-
   }
 
   let settings = SETTINGS.slice(0)
@@ -49,35 +44,32 @@ function HourGlass(time, reference) {
   tickTock(this, settings, this.time)
 
   return this
-
 }
 
-HourGlass.prototype.from = function HourGlass$from(current) {
-
+HourGlass.prototype.from = function HourGlass$from (current) {
   let offset = (current - this.reference) / 1000
 
   this.reference = current
 
   let time = -this.time + offset
-  
+
   this.time = -time
 
   let settings = SETTINGS.slice(0)
 
   tickTock(this, settings, this.time)
-  
+
   return this
 }
 
-HourGlass.prototype.fromNow = function HourGlass$fromNow() {
+HourGlass.prototype.fromNow = function HourGlass$fromNow () {
   return this.from(new Date(Date.now()))
 }
 
-HourGlass.prototype.friendly = function HourGlass$friendly() {
-  let seconds = Math.abs(this.time)
+HourGlass.prototype.friendly = function HourGlass$friendly () {
   let formatted = formatDate(this.reference)
   let string = ''
-  
+
   if (this.years) {
     let years = Math.abs(this.years)
     if (years > MILLION) {
@@ -108,47 +100,39 @@ HourGlass.prototype.friendly = function HourGlass$friendly() {
   }
 
   string += ' From ' + formatted
-  
+
   return string
 
-
-  function formatDate(date) {
+  function formatDate (date) {
     let day = date.getDate().toString()
     let month = (date.getMonth() + 1).toString()
     let year = (date.getFullYear()).toString()
     return month + '/' + day + '/' + year
   }
-  
 }
 
-HourGlass.prototype.valueOf = function HourGlass$valueOf() {
+HourGlass.prototype.valueOf = function HourGlass$valueOf () {
   return (this.reference / 1000) - this.time
 }
 
-HourGlass.prototype.serialize = function HourGlass$seralize() {
+HourGlass.prototype.serialize = function HourGlass$seralize () {
   return this.time.toString() + '|' + this.reference.valueOf()
 }
 
-function createHourGlass(time, reference) {
+function createHourGlass (time, reference) {
   return new HourGlass(time, reference)
 }
- 
-function tickTock(self, settings, remainder) {
 
-  if (settings.length == 0) { return }
+function tickTock (self, settings, remainder) {
+  if (settings.length === 0) { return }
 
   let setting = settings.shift()
 
   if (Math.abs(remainder) >= setting.ratio) {
-
     self[setting.property] = remainder < 0 ? Math.ceil(remainder / setting.ratio) : Math.floor(remainder / setting.ratio)
     remainder = remainder % setting.ratio
-
   } else {
-
-    self[setting.property] = 0 
-
+    self[setting.property] = 0
   }
   tickTock(self, settings, remainder)
 }
-
